41e907ea02b9a82f0f084632f5c8fe5393e1d62b
Blaire Baker <bakerblaire@gmail.com>
2023-02-19 12:23:12 -0500

    add functionality

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk')
 const yargs = require('yargs')
-const getNotes = require('./notes.js')
+const notes = require('./notes.js')
 
 // Customize yargs version
 yargs.version('1.1.0')
@@ -22,8 +22,7 @@ yargs.command({
         }
     },
     handler: function(argv){
-        console.log('Title: ' + argv.title)
-        console.log('Body: ' + argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 })
 
@@ -31,6 +30,13 @@ yargs.command({
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
+    builder : {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
     handler: function(){
         console.log('Removing the note')
     }
