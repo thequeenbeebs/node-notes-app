518c1dcfe470cd1a777e039144bc50d840eca4da
Blaire Baker <bakerblaire@gmail.com>
2023-02-25 14:19:35 -0500

    list notes functionality

--- notes.js
+++ notes.js
@@ -37,6 +37,16 @@ const removeNote = (title) => {
     }
 }
 
+const listNotes = () => {
+    const notes = loadNotes()
+
+    console.log(chalk.magenta.inverse('Your Notes:'))
+    
+    notes.forEach((note) => {
+        console.log(note.title)
+    })
+}
+
 const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
@@ -55,5 +65,6 @@ const loadNotes = () => {
 module.exports = {
     getNotes: getNotes,
     addNote: addNote,
-    removeNote: removeNote
+    removeNote: removeNote,
+    listNotes: listNotes
 }
